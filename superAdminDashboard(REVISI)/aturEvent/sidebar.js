f2e1af5aff4b0fe34efe8cdb6c39ba18c6b5dbf2
DevaK25 <64441851+DevaK25@users.noreply.github.com>
2021-03-04 11:28:33 +0700

    ATUR EVENT (REVISI)
    FINISHED

---
+++ superAdminDashboard(REVISI)/aturEvent/sidebar.js
@@ -0,0 +1,13 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "407px";
+    document.getElementById("main").style.marginLeft = "405px";
+    document.getElementById("after-nav").style.display = "none";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("after-nav").style.display = "block";
+  }
